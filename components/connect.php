d6453619370ad5a64b135803de9084d4bb3a7347
youssef gamal <96102611+engYoussefGamal@users.noreply.github.com>
2022-10-13 03:18:49 +0200

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_name = 'mysql:host=sql211.epizy.com;dbname=epiz_32780519_course_db';
+$user_name = 'epiz_32780519';
+$user_password = 'hBbUE3B273bB';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+function unique_id()
+{
+   $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+   $rand = array();
+   $length = strlen($str) - 1;
+   for ($i = 0; $i < 20; $i++) {
+      $n = mt_rand(0, $length);
+      $rand[] = $str[$n];
+   }
+   return implode($rand);
+}
